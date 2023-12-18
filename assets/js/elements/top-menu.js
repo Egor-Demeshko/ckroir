1b233e396dd21fda25f01a111fabb27975f243c6
Egor Demeshko <demeshko.egor@gmail.com>
2023-12-18 06:52:40 +0300

    top-menu overitems shrink grow

--- assets/js/elements/top-menu.js
+++ assets/js/elements/top-menu.js
@@ -15,25 +15,32 @@ import cleanHTML from "/assets/js/elements/cleanFromScripts.js";
     const ETC = "etc";
     const menu = "menu";
 
-    var menuInnerWidth = 0;
-
+    /**постоянная добавка к ширине, гарантирующая свободное добавление элементов */
+    const INNERMENUGAP = 100;
 
     //1. Получаем доступ к топ меню по айди #menu это будет элемент UL
     const menuDomElement = document.getElementById(menu);
+    /**элемент nav */
+    const menuTopWrapper = menuDomElement.closest('.top-menu');
     const etcDomElement = document.getElementById(ETC);
+    
+    var menuInnerWidth = calculateMenuInnerWidth(menuDomElement);;
 
     if(!menuDomElement || !etcDomElement) return;
 
-    menuInnerWidth = calculateMenuInnerWidth(menuDomElement);
-    /*if(menuDomElement.offsetWidth < menuInnerWidth){*/
-        shrinkMenu();
-    /*}*/
-        setTimeout( () => {
-            growMenu();
-        }, 2000);
+    shrinkIfNeeded(menuDomElement);
+    growIfNeeded(menuDomElement);
 
-    //3. Добавляем новую строку в дом, в список etc, в начало.
-    //4. пересчитываем ширину #menu
+    if(window){
+        window.addEventListener("resize", () => {
+            /**в мобильной версии меню не отображатеся, не делать ничего */
+            if(menuTopWrapper.style.display === "none") return;
+            shrinkIfNeeded(menuDomElement);
+            growIfNeeded(menuDomElement);
+        });
+    }
+    
+
 
     //5. создаем слушателя событий для resize который будет запускать логику выша, а также крутить ее в обратную сторону
 
@@ -42,7 +49,7 @@ import cleanHTML from "/assets/js/elements/cleanFromScripts.js";
     //6.2 удалять из ДОМ
     //6.3 в строке менять классы
 
-    function calculateMenuInnerWidth(menuDomElement){
+    function calculateMenuInnerWidth(){
         //2. Бежим по li вернхнего уровня, с классом "top-menu__list-item", считаем ширину элементов в сумму
         const elements = getTopMenuElements();
         var width = 0;
@@ -55,7 +62,7 @@ import cleanHTML from "/assets/js/elements/cleanFromScripts.js";
         return width;
     }
 
-
+    
     function getTopMenuElements(){
         const elements = menuDomElement.querySelectorAll(`.${topMenuClass}`);
         return elements;
@@ -86,9 +93,34 @@ import cleanHTML from "/assets/js/elements/cleanFromScripts.js";
         }
     }
 
+    /**сравниваем ширину внутренних элементов видимой части верхнего меню, с общим треком верхнего меню,    */
+    function growIfNeeded(menuDomElement){
+        console.log("GROWIFNEEDED START");
+        if(menuDomElement.offsetWidth > menuInnerWidth + INNERMENUGAP){
+            while(menuDomElement.offsetWidth > menuInnerWidth + INNERMENUGAP){
+                growMenu();
+                menuInnerWidth = calculateMenuInnerWidth();
+                console.log("GROWIFNEEDED LOOP", menuInnerWidth);
+            }
+        }
+    }
+
+    /**сравниваем ширину внутренних элементов видимой части верхнего меню, с общим треком меню */
+    function shrinkIfNeeded(menuDomElement){
+        console.log("SHRINKIFNEEDED START");
+        if(menuDomElement.offsetWidth < menuInnerWidth){
+            while(menuDomElement.offsetWidth < menuInnerWidth){
+                shrinkMenu();
+                menuInnerWidth = calculateMenuInnerWidth();
+                console.log("SHRINKIFNEEDED LOOP", menuInnerWidth);
+            }
+        } 
+    }
+
     //   2.1 если ширина элементов превосходит ширину #menu, то хватаем последний элемент с классом "top-menu__list-item"
     //   и превращаем  его в строку. Удаляем в дом. 
     //   2.2 меняем классы "right" и "left" и остальные
+    //3. Добавляем новую строку в дом, в список etc, в начало.
     function shrinkMenu(){
         const topElements = getTopMenuElements();
         /**-2 так как последний элемент это "etc" */
